4cf0cd74cfdb5ecaf78f11985b4db7546d382300
unknown <ravi943117@gmail.com>
2022-10-06 17:38:54 +0530

    javascript data comment out

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,4 +1,4 @@
-const todo =document.querySelector(".todo");
-todo.style.cssText = `color:blue; font-size:20px`
-console.log("this is my todolist");
+// const todo =document.querySelector(".todo");
+// todo.style.cssText = `color:blue; font-size:20px`
+// console.log("this is my todolist");
 
